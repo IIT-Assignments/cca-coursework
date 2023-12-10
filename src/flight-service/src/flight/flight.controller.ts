9b2354c6cbe87b398a503a62c3dd767fcef6d03a
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-12-10 16:24:15 +0530

    implemented create and get flights

--- src/flight-service/src/flight/flight.controller.ts
+++ src/flight-service/src/flight/flight.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { FlightService } from './flight.service';
+import { Flight } from './schemas/flight.schema';
 
-@Controller('flight')
-export class FlightController {}
+@Controller('flights')
+export class FlightController {
+  constructor(private flightService: FlightService) {}
+
+  @Get()
+  async getFlights(): Promise<Flight[]> {
+    return this.flightService.findAll();
+  }
+
+  @Post()
+  async createFlight(
+    @Body()
+    flight,
+  ): Promise<Flight> {
+    return this.flightService.create(flight);
+  }
+}
--- src/flight-service/src/flight/flight.service.ts
+++ src/flight-service/src/flight/flight.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { Flight } from './schemas/flight.schema';
 
 @Injectable()
-export class FlightService {}
+export class FlightService {
+  constructor(
+    @InjectModel(Flight.name)
+    private flightModal: mongoose.Model<Flight>,
+  ) {}
+
+  async findAll(): Promise<Flight[]> {
+    const flights = await this.flightModal.find();
+    return flights;
+  }
+
+  async create(flight: Flight): Promise<Flight> {
+    const newFlight = await this.flightModal.create(flight);
+    return newFlight;
+  }
+}
